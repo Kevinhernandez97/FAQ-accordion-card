eaa1aa95d4a34a3af9976fe6c05fd3b977a8bcee
Kevinhernandez97 <kmhs-18@hotmail.com>
2022-11-04 10:43:16 -0500

    reorganizacion de carpetas

---
+++ main.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll(".main__container-question");
+const $items = [...items];
+
+$items.map((item) => {
+    const addActive = () => {
+        $items.map((i) => {
+            if(!(item === i)) {
+                i.classList.remove("active");
+            }
+        })
+
+        item.classList.toggle("active");
+    }
+
+    item.addEventListener('click', addActive)
+});
